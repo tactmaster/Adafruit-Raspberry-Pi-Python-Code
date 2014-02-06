b45ee09a5e31b9f626a1b5e7591d23955961e420
Ed Watson <edmundwatson@gmail.com>
2014-02-06 22:36:09 +0000

    Adding clock example

--- Adafruit_CharLCDPlate/Adafruit_CharLCDPlate.py
+++ Adafruit_CharLCDPlate/Adafruit_CharLCDPlate.py
@@ -439,7 +439,7 @@ class Adafruit_CharLCDPlate(Adafruit_I2C):
 
 if __name__ == '__main__':
 
-    lcd = Adafruit_CharLCDPlate()
+    lcd = Adafruit_CharLCDPlate(busnum = 1)
     lcd.begin(16, 2)
     lcd.clear()
     lcd.message("Adafruit RGB LCD\nPlate w/Keypad!")
